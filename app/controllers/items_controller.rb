f5bf14054a4c044f9a9035c5c15928d725eb3503
akiduki0526 <akiduki0526@gmail.com>
2022-12-23 11:06:41 +0900

    商品出品機能#5修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,15 +5,22 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
+
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+
   end
   
   def create
-    @item = Item.create(item_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
       render :new
     end
+
+
   end
 
   private
