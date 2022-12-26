e5b5e29cb8b1c702e643129635cc0fec982fff1f
akiduki0526 <akiduki0526@gmail.com>
2022-12-26 10:40:31 +0900

    商品削除機能#9

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new,:edit]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:new,:edit,:destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.order(id: "DESC")
@@ -39,6 +39,13 @@ class ItemsController < ApplicationController
     end
   end
 
+
+  def destroy
+   @item.destroy
+   if @item.destroy
+    redirect_to root_path
+   end
+  end
 
 
 
